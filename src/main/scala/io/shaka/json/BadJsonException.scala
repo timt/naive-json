7034e0fbf5da65be25633a12465dc78112c2a24f
Tim Tennant <ttennant@gmail.com>
2014-05-10 20:17:40 +0100

    Remove comment

--- src/main/scala/io/shaka/json/BadJsonException.scala
+++ src/main/scala/io/shaka/json/BadJsonException.scala
@@ -1,6 +1,3 @@
 package io.shaka.json
 
-/**
- * Created by timt on 09/05/2014.
- */
 class BadJsonException(json: String, t: Throwable) extends RuntimeException(s"Bad JSON, can not parse: $json", t)
