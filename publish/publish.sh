b91c8f71ab37a12ce697623af6866f827fab705f
Tim Tennant <ttennant@gmail.com>
2014-05-19 07:38:14 +0100

    publish

--- publish/publish.sh
+++ publish/publish.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-cd $HOME
-pwd
-echo 'hello $HOME'
 cd $HOME/publish
+
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 git clone --quiet https://${GH_TOKEN}@github.com/timt/timt.github.com.git > /dev/null
-cd $HOME
+
+cp -R releases/ timt.github.com/repo/releases/
+cd timt.github.com
+git add -f .
+git commit -m "Lastest json lib on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to repo"
+git push -fq origin master > /dev/null
+
+echo -e "Published json lib to repo.\n"
