bfac34ccd29475515c419ef7ad9d7e6fef51666a
travis-ci <travis@travis-ci.org>
2014-05-19 18:08:46 +0100

    fix repo path

--- publish/publish.sh
+++ publish/publish.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-echo -e "I got here"
 cd $TRAVIS_BUILD_DIR/publish
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 git clone --quiet https://${GH_TOKEN}@github.com/timt/timt.github.com.git > /dev/null
 
-cp -R releases/ timt.github.com/repo/releases/
+cp -R releases/ timt.github.com/repo/
 cd timt.github.com
 git add -f .
 git commit -m "Lastest json lib on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to repo"
