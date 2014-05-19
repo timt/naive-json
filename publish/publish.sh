85730f0af1ed299ea911f0bad4338435cb5122ed
Tim Tennant <ttennant@gmail.com>
2014-05-19 07:26:40 +0100

    clone the repo

--- publish/publish.sh
+++ publish/publish.sh
@@ -1,4 +1,9 @@
-#!/bash/bin
+#!/bin/bash
 cd $HOME
 pwd
 echo 'hello $HOME'
+cd $HOME/publish
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet https://${GH_TOKEN}@github.com/timt/timt.github.com.git > /dev/null
+cd $HOME
