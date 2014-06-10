67b6245d9487abd4241544396da49ffa3cb64d98
Tim Tennant <ttennant@gmail.com>
2014-06-10 07:05:28 +0100

    Mov JsonTokenizer into production code

---
+++ src/main/scala/io/shaka/json/Token.scala
@@ -0,0 +1,3 @@
+package io.shaka.json
+
+case class Token(value: String)
--- src/test/scala/io/shaka/json/TokenizerSpec.scala
+++ src/test/scala/io/shaka/json/TokenizerSpec.scala
@@ -50,55 +50,11 @@ class TokenizerSpec extends Spec {
     assert(tokens === List(Token("["), Token("\"sheep\""), Token(","), Token("\"cheese\""), Token(","), Token("123.456"), Token(","), Token("{"), Token("\"greeting\""), Token(":"), Token("\"sheep & cheese!\""), Token("}"), Token("]")))
   }
 
-
-
 }
 
-case class Token(value: String)
 
-object JsonTokenizer {
-  val leftBrace = TokenMatcher("\\{")
-  val rightBrace = TokenMatcher("\\}")
-  val leftBracket = TokenMatcher("\\[")
-  val rightBracket = TokenMatcher("\\]")
-  val colon = TokenMatcher(":")
-  val comma = TokenMatcher(",")
-  val isTrue = TokenMatcher("true")
-  val isFalse = TokenMatcher("false")
-  val isNull = TokenMatcher("null")
-  val string = TokenMatcher("\".*?\"")
-  val number = TokenMatcher("[0-9]+[\\,\\.][0-9]+")
 
-  class TokenMatcher(partialRegex: String) {
-    private val regex = s"^($partialRegex)".r
 
-    def unapply(string: String) = regex.findFirstIn(string)
-  }
-
-  object TokenMatcher {
-    def apply(partialRegex: String) = new TokenMatcher(partialRegex)
-  }
-
-  def tokenize(json: String, tokens: List[Token] = List()): List[Token] = {
-    val trimmedJson = json.trim
-    def continue(matched: String) = tokenize(trimmedJson.substring(matched.length), Token(matched) :: tokens)
-    trimmedJson match {
-      case "" => tokens.reverse
-      case leftBrace(s) => continue(s)
-      case rightBrace(s) => continue(s)
-      case leftBracket(s) => continue(s)
-      case rightBracket(s) => continue(s)
-      case colon(s) => continue(s)
-      case comma(s) => continue(s)
-      case isTrue(s) => continue(s)
-      case isFalse(s) => continue(s)
-      case isNull(s) => continue(s)
-      case string(s) => continue(s)
-      case number(s) => continue(s)
-      case oops => println(s"oops!!!! seems we didn't deal with --- $oops --- tokens :("); tokens.reverse
-    }
-  }
-}
 
 
 
